dd3fccba83a2d84c651b2941ad71eb8c773e950d
tommyjade4ssu <tommyjade@ssu.ac.kr>
2026-04-02 17:27:44 +0900

    dev2

--- mp05_kakao/src/main/java/com/example/mp05_kakao/MainActivity.kt
+++ mp05_kakao/src/main/java/com/example/mp05_kakao/MainActivity.kt
@@ -11,10 +11,98 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ){
+            if(it.all { permission -> permission.value }){
+                noti()
+            }else {
+                Toast.makeText(this,"permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+        binding.notificationButton.setOnClickListener {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                val grant = ContextCompat.checkSelfPermission(
+                    this,
+                    "android.permission.POST_NOTIFICATIONS"
+                )
+                if( grant == PackageManager.PERMISSION_GRANTED){
+                    noti()
+                }else {
+                    permissionLauncher.launch(arrayOf("android.permission.POST_NOTIFICATIONS"))
+                }
+            }else {
+                noti()
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+        fun noti(){
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val builder: NotificationCompat.Builder
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channelId = "one-channel"
+            val channelName = "My Channel One"
+            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+                .apply {
+                    description = "My Channel One Description"
+                    setShowBadge(true)
+                    val uri : Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+                    val audioAttributes = AudioAttributes.Builder()
+                        .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                        .setUsage(AudioAttributes.USAGE_ALARM)
+                        .build()
+                    setSound(uri, audioAttributes)
+                    enableVibration(true)
+                }
+            manager.createNotificationChannel(channel)
+            builder = NotificationCompat.Builder(this, channelId)
+
+        }else {
+            builder = NotificationCompat.Builder(this)
+        }
+
+        builder.run {
+            setSmallIcon(android.R.drawable.btn_default_small)
+            setWhen(System.currentTimeMillis())
+            setContentTitle("홍길동")
+            setContentText("안녕하세요.")
+            setLargeIcon(BitmapFactory.decodeResource(resources, android.R.drawable.star_big_on))
+        }
+
+        val KEY_TEXT_REPLY = "key_text_reply"
+        var replyLabel = "답장"
+        val remoteInput: RemoteInput = RemoteInput.Builder(KEY_TEXT_REPLY).run {
+            setLabel(replyLabel)
+            build()
+        }
+        val replyIntent = Intent(this, MyReceiver::class.java)
+        val replyPendingIntent = PendingIntent.getBroadcast(
+            this, 30, replyIntent, PendingIntent.FLAG_MUTABLE
+        )
+
+        builder.addAction(
+            NotificationCompat.Action.Builder(
+                android.R.drawable.ic_menu_send, "답장", replyPendingIntent
+            ).addRemoteInput(remoteInput).build()
+        )
+        builder.setProgress(100, 0, false)
+        manager.notify(11, builder.build())
+        thread {
+            for (i in 1..100) {
+                builder.setProgress(100, i, false)
+                manager.notify(11, builder.build())
+                SystemClock.sleep(100)
+            }
         }
     }
 }
